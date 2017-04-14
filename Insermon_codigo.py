6424af85a661e8f450f7c9a3514cf950de27316b
pesuarez <pedrofcsuarez@uol.com.br>
2017-04-14 13:19:21 -0300

    primeira fase

--- Insermon_codigo.py
+++ Insermon_codigo.py
@@ -1,30 +1,56 @@
-from funcao_batalha import batalha
-
-import json
-with open('dicionario.json') as arquivo:
-	pokedex = json.load(arquivo)
-
+print(" ")
+print("0 - Charmander")
+print("1 - Bulbassauro")
+print("2 - Squirtle")
+print(" ")
+escolha = int(input("Escolha um dos pokemons iniciais: "))
+pokemon=pokedex[escolha]
+np=pokemon["nome"]
+pp=pokemon["ataque"]
+vp=pokemon["vida"]
+dp=pokemon["defesa"]
+vida_max=vp
 while True:
-	print("Charmander")
-	print("Bulbassauro")
-	print("Squirtle")
-	escolha = input("Escolha um dos pokemons iniciais. ")
-	pokemon=pokedex[escolha]
-	pp=pokemon["poder"]
-	vp=pokemon["vida"]
-	dp=pokemon["defesa"]
-	pergunta=input("Escolha se quer passear ou se quer dormir. ")
-	if pergunta=="dormir":
-		break
-	elif pergunta=="passear":
-		x = random.choice(pokedex)
-		print(x)		
-		p=x["poder"]
+	print(" ")
+	print("O seu pokemon é {0}, vida: {1}, ataque: {2}, defesa: {3}.".format(np,vp,pp,dp))
+	print(" ")
+	print("1 - Passear")
+	print("2 - Dormir")
+	pergunta=int(input("Escolha o que você quer fazer: "))
+	print("  ")
+	if pergunta==2:
+		print("Ok, até a próxima!")
+	elif pergunta==1:
+		print("Você encontrou um oponente!")
+		x = randint.choice(pokedex)
+		n=x["nome"]
+		p=x["ataque"]
 		v=x["vida"]
 		d=x["defesa"]
-		batalha(p,v,d)
+		print("O seu oponente é {0}, vida: {1}, ataque: {2}, defesa: {3}.".format(n,v,p,d))
+		print(" ")
+		while vp > 0 and v > 0:
+			print("Sua vida é {0}".format(vp))
+			print("A vida do oponente é {0}".format(v))
+			print(" ")
+			seuataque=(pp-d)
+				if seuataque<0:
+					seuataque=0
+				v=v-seuataque
+				if v<=0:
+					v=0
+					break
+				opoataque=(p*-dp*)	
+				if opoataque<0:
+					opoataque=0	
+				vp=vp-opoataque
+				if vp<=0:
+					vp=0
+					break
 		if vp==0:
-			print("Você perdeu. ")
-			break
+				print(" ")
+				print("Seu pokemon morreu. ")
+				print("Game over!")
 		elif v==0:
-			print("Parabens! ")
+			print(" ")
+			print("Parabéns, você ganhou a luta! ")
